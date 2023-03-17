a1c2137aba23fad39405268bafd95bfc57fae175
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-03-17 08:06:51 +0200

    Fixing files

--- client/src/components/Details/Content/Function.js
+++ client/src/components/Details/Content/Function.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { unknownSVG } from './SvgImports';
+import { unknownSVG } from './SvgImport';
 import './Functions.css'
 
 const unknown = <img className='svg unknown' src={unknownSVG} />;
