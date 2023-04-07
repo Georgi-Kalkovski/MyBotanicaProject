657a660e9fc880a67773d112ff6728c00a54c245
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-04-07 13:47:22 +0300

    Update species.routes.js

--- server/routes/species.routes.js
+++ server/routes/species.routes.js
@@ -4,7 +4,6 @@ const speciesController = require('../controllers/species.controller');
 const router = express.Router();
 
 router.get('/', speciesController.getAllSpecies);
-router.get('/species/:scientific_name'.replace(/ /g, '_'), speciesController.getPlantByScientificName);
-
+router.get('/species/:scientific_name'.split(' ').join('_'), speciesController.getPlantByScientificName);
 
 module.exports = router;
