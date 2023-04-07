f6f679c90b33c8c406bc5a47981ceec033161f90
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-04-07 12:08:24 +0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT: process.env.PORT,
+    PORT: process.env.PORT || 'https://mybotanica-api.onrender.com/',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://Terter:Terter@botanica.s3drjky.mongodb.net/BotanicaDB?retryWrites=true&w=majority',
   };
